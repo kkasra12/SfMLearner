5f39b53680cbeda2f209399f701d12b7e173ec86
kkasra12 <kkasra12@gmail.com>
2021-06-03 10:47:06 +0430

    define num_source flag

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 from __future__ import division
-import tensorflow as tf
+
+import os
 import pprint
 import random
+
 import numpy as np
+import tensorflow as tf
+
 from SfMLearner import SfMLearner
-import os
 
 flags = tf.app.flags
 flags.DEFINE_string("dataset_dir", "", "Dataset directory")
@@ -20,10 +23,12 @@ flags.DEFINE_integer("img_width", 416, "Image width")
 flags.DEFINE_integer("seq_length", 3, "Sequence length for each example")
 flags.DEFINE_integer("max_steps", 200000, "Maximum number of training iterations")
 flags.DEFINE_integer("summary_freq", 100, "Logging every log_freq iterations")
+flags.DEFINE_integer("num_source", None, "Number of source images")
 flags.DEFINE_integer("save_latest_freq", 5000, \
-    "Save the latest model every save_latest_freq iterations (overwrites the previous latest model)")
+                     "Save the latest model every save_latest_freq iterations (overwrites the previous latest model)")
 flags.DEFINE_boolean("continue_train", False, "Continue training from previous checkpoint")
 FLAGS = flags.FLAGS
+
 
 def main(_):
     seed = 8964
@@ -33,12 +38,13 @@ def main(_):
 
     pp = pprint.PrettyPrinter()
     pp.pprint(flags.FLAGS.__flags)
-    
+
     if not os.path.exists(FLAGS.checkpoint_dir):
         os.makedirs(FLAGS.checkpoint_dir)
-        
+
     sfm = SfMLearner()
     sfm.train(FLAGS)
+
 
 if __name__ == '__main__':
     tf.app.run()
